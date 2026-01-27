b3f615adcf913514b18e00e2f37ce58e41d03a21
DiCode77 <dimazamshaq@icloud.com>
2026-01-27 23:24:33 +0200

    This file is required for making corrections.

---
+++ ActiveLight/src/Redefinition.hpp
@@ -0,0 +1,7 @@
+//
+//  Redefinition.hpp
+//  ActiveLight
+//
+//  Created by DiCode77.
+//
+// In this file, you need to make your own edits to the names of cities, which will be automatically used.
